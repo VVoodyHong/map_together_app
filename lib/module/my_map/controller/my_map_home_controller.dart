a1a363bef906b177c720bdae6bd1567740ea4e9d
jwhong <hju4287@naver.com>
2022-03-29 12:03:25 +0900

    add main

---
+++ lib/module/my_map/controller/my_map_home_controller.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+class MyMapHomeX extends GetxController {
+  static MyMapHomeX get to => Get.find();
+
+  // @override
+  // void onInit() {
+  //   super.onInit();
+  // }
+}
