50c8679791aa2083db79d467e727081e6eec6dd7
jwhong <hju4287@naver.com>
2022-03-29 12:03:58 +0900

    add comstants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MtColor {
+  static const signature = Color(0xFF2DB9F3);
+  static const black = Colors.black;
+  static const white = Colors.white;
+  static const grey = Colors.grey;
+}
