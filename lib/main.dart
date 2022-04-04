39da43ff97d648cd58017c04e4f2ae07bd4b6ab3
jwhong <hju4287@naver.com>
2022-04-04 13:47:03 +0900

    modify theme color

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,31 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:map_together/app.dart';
 import 'package:map_together/module/login/splash_screen.dart';
 import 'package:map_together/navigator/ui_logic.dart';
+import 'package:map_together/utils/constants.dart';
 import 'package:map_together/widget/bottom_nav.dart';
 
 void main() {
   runApp(MapTogether());
+  if(Platform.isAndroid) {
+    SystemChrome.setSystemUIOverlayStyle( SystemUiOverlayStyle(
+      statusBarColor: MtColor.white,
+      statusBarIconBrightness: Brightness.dark
+    ));
+  }
 }
 
 class MapTogether extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+        theme: ThemeData(
+          scaffoldBackgroundColor: MtColor.white
+        ),
         initialBinding: BindingsBuilder(() {
           Get.put(App());
           Get.put(NavigatorX());
