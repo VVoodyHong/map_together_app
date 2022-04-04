cd6acd7ac87c6ab3ca7cdb22819ede108b11fca7
jwhong <hju4287@naver.com>
2022-04-04 13:50:00 +0900

    add btn profile widget

---
+++ lib/widget/btn_profile.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:map_together/utils/constants.dart';
+
+class BtnProfile extends StatelessWidget {
+
+  final String title;
+  final String number;
+
+  BtnProfile({
+    required this.title,
+    required this.number
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Expanded(
+      child: InkWell(
+        onTap: () {
+          print('$title tapped');
+        },
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 10),
+          child: Column(
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: FontSize.medium,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox( height: 10 ),
+              Text(
+                number,
+                style: TextStyle(
+                  fontSize: FontSize.medium
+                ),
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
