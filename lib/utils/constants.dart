759d317f9881f5874f2749d8fbd0e107f22a14bc
jwhong <hju4287@naver.com>
2022-04-04 13:49:52 +0900

    add constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 class MtColor {
   static const signature = Color(0xFF2DB9F3);
   static const black = Colors.black;
+  static const paleBlack = Colors.black54;
   static const white = Colors.white;
   static const grey = Colors.grey;
+  static const paleGrey = Color(0xFFEEEEEE);
+}
+
+class FontSize {
+  static const small = 12.0;
+  static const medium = 16.0;
+  static const large = 20.0;
+}
+
+class Asset {
+  static const profile = 'lib/assets/images/profile.jpg';
 }
