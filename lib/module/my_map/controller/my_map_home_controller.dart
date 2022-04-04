347fedde438f5339dd37ec326caba5899f7b0031
jwhong <hju4287@naver.com>
2022-04-04 13:47:45 +0900

    add my map home screen

--- lib/module/my_map/controller/my_map_home_controller.dart
+++ lib/module/my_map/controller/my_map_home_controller.dart
@@ -1,10 +1,44 @@
+import 'dart:async';
+
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:get/get.dart';
+import 'package:map_together/utils/utils.dart';
 
 class MyMapHomeX extends GetxController {
   static MyMapHomeX get to => Get.find();
 
-  // @override
-  // void onInit() {
-  //   super.onInit();
-  // }
+  Completer<NaverMapController> mapController = Completer();
+
+  RxBool createMode = false.obs;
+
+  Rx<CameraPosition> cameraPosition = CameraPosition(
+    target: LatLng(35.94841985643522, 127.68575755041469),
+    zoom: 5.7,
+  ).obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  void onMapCreated(NaverMapController controller) {
+    if (mapController.isCompleted) mapController = Completer();
+    mapController.complete(controller);
+  }
+
+  void onMapTap(LatLng position) {
+    if(createMode.value) {
+      Utils.showToast('mapTap\n' + position.toString());
+    }
+  }
+
+  void onSymbolTap(LatLng? position, String? caption) {
+    if(createMode.value) {
+      Utils.showToast('symbolTap\n' + position.toString() + '\n' + caption.toString());
+    }
+  }
+
+  void changeCreateMode() {
+    createMode.value = !createMode.value;
+  }
 }
--- lib/module/my_map/view/my_map_home_screen.dart
+++ lib/module/my_map/view/my_map_home_screen.dart
@@ -1,14 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:get/get.dart';
 import 'package:map_together/module/my_map/controller/my_map_home_controller.dart';
+import 'package:map_together/utils/constants.dart';
+import 'package:map_together/utils/utils.dart';
 import 'package:map_together/widget/bottom_nav.dart';
+import 'package:map_together/widget/header_bar.dart';
+import 'package:map_together/widget/profile.dart';
 
 class MyMapHomeScreen extends GetView<MyMapHomeX> {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("My Map")),
+    return Obx(() => Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            HeaderBar(),
+            Profile(),
+            _myMap()
+          ],
+        ),
+      ),
+      floatingActionButton: _floatingActionButton(),
       bottomNavigationBar: BottomNav(),
+    ));
+  }
+
+  _floatingActionButton() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 10),
+      child: FloatingActionButton(
+        elevation: 2,
+        backgroundColor: MtColor.signature,
+        child: Icon(
+          !controller.createMode.value ? Icons.add : Icons.clear,
+          color: MtColor.white,
+          size: 35,
+        ),
+        onPressed: _onPressButton,
+      ),
+    );
+  }
+
+  _onPressButton() {
+    controller.changeCreateMode();
+    if(controller.createMode.value) {
+      Utils.showToast('추가할 장소를 선택해주세요.');
+    }
+  }
+
+  _myMap() {
+    return Expanded(
+      child: NaverMap(
+        initialCameraPosition: controller.cameraPosition.value,
+        locationButtonEnable: true,
+        onMapCreated: controller.onMapCreated,
+        onMapTap: controller.onMapTap,
+        onSymbolTap: controller.onSymbolTap,
+      ),
     );
   }
 }
