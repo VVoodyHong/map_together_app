62c5e31bc5161e96187954355a7ce4831f2a6940
jwhong <hju4287@naver.com>
2022-04-05 14:50:59 +0900

    add appbar backbutton widget

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get.dart';
+import 'package:map_together/navigator/ui_logic.dart';
+import 'package:map_together/navigator/ui_state.dart';
 import 'package:map_together/utils/constants.dart';
 
 class Utils {
@@ -11,6 +15,22 @@ class Utils {
       backgroundColor: MtColor.paleBlack,
       textColor: MtColor.white,
       fontSize: FontSize.medium
+    );
+  }
+
+  static void moveTo(UiState state, {dynamic arg}) {
+    UiLogic.changeUiState(
+      state,
+      prevState: UiLogic.getUiState(),
+      arg: arg
+    );
+  }
+
+  static IconButton appBarBackButton({Color? color, VoidCallback? onPressed}) {
+    return IconButton(
+      onPressed: onPressed ?? () => Get.close(1),
+      color: color ?? MtColor.black,
+      icon: Icon(Icons.arrow_back)
     );
   }
 }
