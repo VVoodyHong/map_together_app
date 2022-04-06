a0a94f52365cf13cc1d6302d3a0fe9eb29de9f9c
jwhong <hju4287@naver.com>
2022-04-06 14:28:54 +0900

    refactor reverse geocodiing api

--- lib/rest/api_request.dart
+++ lib/rest/api_request.dart
@@ -17,16 +17,12 @@ class API extends GetConnect {
   }
 
   // https://api.ncloud-docs.com/docs/ai-naver-mapsreversegeocoding-gc
-  Future<Response<dynamic>> test(lon ,lat) async {
+  Future<Response<dynamic>> reverseGeocoding(lon ,lat) async {
     // httpClient.defaultDecoder = (map) => map['data'];
-    Map<String,String> headers = {
+    Map<String, String> headers = {
       "X-NCP-APIGW-API-KEY-ID": naverClientId, // 개인 클라이언트 아이디
       "X-NCP-APIGW-API-KEY": naverClientSecret // 개인 시크릿 키
     };
-    httpClient.addAuthenticator((Request request) async {
-      request.headers.addAll(headers);
-      return request;
-    });
-    return await httpClient.get('https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?request=coordsToaddr&coords=${lon},${lat}&sourcecrs=epsg:4326&orders=admcode,legalcode,addr,roadaddr&output=json');
+    return await httpClient.get('https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?request=coordsToaddr&coords=${lon},${lat}&sourcecrs=epsg:4326&orders=addr&output=json', headers: headers);
   }
 }
