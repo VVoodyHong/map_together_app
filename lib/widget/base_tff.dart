f98ca8d4fb7c989e750099c23304f9e964e529ee
jwhong <hju4287@naver.com>
2022-04-08 12:51:39 +0900

    update base tff

--- lib/widget/base_tff.dart
+++ lib/widget/base_tff.dart
@@ -3,6 +3,7 @@ import 'package:map_together/utils/constants.dart';
 
 class BaseTextFormField extends StatelessWidget {
   final String? hintText;
+  final String? label;
   final TextEditingController? controller;
   final Icon? suffixIcon;
   final bool? enabled;
@@ -11,6 +12,7 @@ class BaseTextFormField extends StatelessWidget {
 
   BaseTextFormField({
     this.hintText,
+    this.label,
     this.controller,
     this.suffixIcon,
     this.enabled,
@@ -22,33 +24,45 @@ class BaseTextFormField extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(vertical: 5),
-      child: TextFormField(
-        enabled: enabled,
-        controller: controller,
-        textAlign: TextAlign.start,
-        maxLines: multiline == true ? null : 1,
-        keyboardType: multiline == true ? TextInputType.multiline: TextInputType.text,
-        decoration: InputDecoration(
-          enabledBorder: UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: MtColor.paleGrey
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if(label != null) Text(
+            label ?? '',
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              fontWeight: FontWeight.w500
             )
           ),
-          focusedBorder: UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: MtColor.signature
-            )
+          TextFormField(
+            enabled: enabled,
+            controller: controller,
+            textAlign: TextAlign.start,
+            maxLines: multiline == true ? null : 1,
+            keyboardType: multiline == true ? TextInputType.multiline: TextInputType.text,
+            decoration: InputDecoration(
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: MtColor.paleGrey
+                )
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: MtColor.signature
+                )
+              ),
+              hintText: hintText,
+              hintStyle: TextStyle(
+                color: MtColor.grey
+              ),
+              suffixIcon: suffixIcon != null ? IconButton(
+                onPressed: onPressedIcon,
+                icon: suffixIcon!
+              ) : null
+            ),
+            cursorColor: MtColor.signature,
           ),
-          hintText: hintText,
-          hintStyle: TextStyle(
-            color: MtColor.grey
-          ),
-          suffixIcon: suffixIcon != null ? IconButton(
-            onPressed: onPressedIcon,
-            icon: suffixIcon!
-          ) : null
-        ),
-        cursorColor: MtColor.signature,
+        ],
       ),
     );
   }
