36b5c61c589ef0224b147d266c504276b346f124
jwhong <hju4287@naver.com>
2022-04-08 12:51:10 +0900

    Update url arg

--- lib/rest/api_request.dart
+++ lib/rest/api_request.dart
@@ -1,5 +1,4 @@
 import 'package:get/get.dart';
-import 'package:get/get_connect/http/src/request/request.dart';
 import 'package:map_together/auth/secrets.dart';
 
 class API extends GetConnect {
@@ -23,6 +22,6 @@ class API extends GetConnect {
       "X-NCP-APIGW-API-KEY-ID": naverClientId, // 개인 클라이언트 아이디
       "X-NCP-APIGW-API-KEY": naverClientSecret // 개인 시크릿 키
     };
-    return await httpClient.get('https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?request=coordsToaddr&coords=${lon},${lat}&sourcecrs=epsg:4326&orders=addr&output=json', headers: headers);
+    return await httpClient.get('https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?request=coordsToaddr&coords=$lon,$lat&sourcecrs=epsg:4326&orders=addr&output=json', headers: headers);
   }
 }
