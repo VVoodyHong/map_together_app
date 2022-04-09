76f21c64b3a890250eff78ffad99191789b974ec
jwhong <hju4287@naver.com>
2022-04-09 20:56:39 +0900

    add base button widget

---
+++ lib/widget/base_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:map_together/utils/constants.dart';
+
+class BaseButton {
+  
+  static IconButton iconButton({
+    required IconData iconData,
+    required Function() onPressed
+  }) {
+    return IconButton(
+      icon: Icon(
+        iconData,
+        color: MtColor.black,
+      ),
+      splashRadius: 24,
+      onPressed: onPressed,
+    );
+  }
+
+  static IconButton textButton({
+    required String text,
+    required Function() onPressed
+  }) {
+    return IconButton(
+      icon: Text(
+        text,
+        style: TextStyle(
+          color: MtColor.black,
+        )
+      ),
+      splashRadius: 24,
+      onPressed: onPressed,
+    );
+  }
+}
