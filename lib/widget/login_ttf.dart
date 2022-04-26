d506286417498418862e9d6a7a767e7c160a91cc
jwhong <hju4287@naver.com>
2022-04-26 18:12:49 +0900

    Add login ttf widget

---
+++ lib/widget/login_ttf.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:map_together/utils/constants.dart';
+
+class LoginTextFormField extends StatelessWidget {
+  final String? hintText;
+  final TextEditingController? controller;
+  final bool? multiline;
+  final bool? obscureText;
+
+  LoginTextFormField({
+    this.hintText,
+    this.controller,
+    this.multiline,
+    this.obscureText
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFormField(
+            controller: controller,
+            textAlign: TextAlign.start,
+            obscureText: obscureText ?? false,
+            decoration: InputDecoration(
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: MtColor.paleGrey
+                )
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: MtColor.signature
+                )
+              ),
+              hintText: hintText,
+              hintStyle: TextStyle(
+                color: MtColor.grey
+              )
+            ),
+            cursorColor: MtColor.signature,
+          ),
+        ],
+      ),
+    );
+  }
+  
+}
