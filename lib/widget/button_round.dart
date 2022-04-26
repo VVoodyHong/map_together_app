77f5372b1a765bae98db913b6b599f80e9426b0c
jwhong <hju4287@naver.com>
2022-04-26 18:12:55 +0900

    Add button round widget

---
+++ lib/widget/button_round.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:map_together/utils/constants.dart';
+
+class ButtonRound extends StatelessWidget {
+
+  final String label;
+  final VoidCallback onTap;
+  final Color? buttonColor;
+  final Color? textColor;
+
+  ButtonRound({
+    required this.label,
+    required this.onTap,
+    this.buttonColor,
+    this.textColor
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      highlightColor: Colors.transparent,
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 15),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: buttonColor ?? MtColor.signature,
+        ),
+        child: Text(
+          label,
+          style: TextStyle(
+            color: textColor ?? Colors.white,
+            fontSize: 16
+          ),
+        ),
+      ),
+    );
+  }
+}
