7d6b8a1af2fe2ca222fde02fcd6dae968b623d5e
jwhong <hju4287@naver.com>
2022-04-27 19:21:34 +0900

    Add search page app bar

--- lib/module/search/view/search_home_screen.dart
+++ lib/module/search/view/search_home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:map_together/module/search/controller/search_home_controller.dart';
+import 'package:map_together/widget/base_app_bar.dart';
 import 'package:map_together/widget/bottom_nav.dart';
 
 class SearchHomeScreen extends GetView<SearchHomeX> {
@@ -8,6 +9,11 @@ class SearchHomeScreen extends GetView<SearchHomeX> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: BaseAppBar(
+        title: '검색',
+        titleWeight: FontWeight.bold,
+        centerTitle: false,
+      ).init(),
       body: SafeArea(
         child: Container()
       ),
