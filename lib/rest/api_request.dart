76bf82bb7f3ab1f83ef5cbb39738613abbd2ccb2
jwhong <hju4287@naver.com>
2022-04-27 19:22:18 +0900

    Add get kakao accout api

--- lib/rest/api_request.dart
+++ lib/rest/api_request.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
+import 'package:get/get_connect/http/src/request/request.dart';
 import 'package:map_together/auth/secrets.dart';
+import 'package:map_together/model/KakaoAccount.dart';
 import 'package:map_together/model/request/user_create.dart';
 import 'package:map_together/rest/api_keys.dart';
 
@@ -21,10 +23,20 @@ class API extends GetConnect {
   Future<Response<dynamic>> reverseGeocoding(lon ,lat) async {
     // httpClient.defaultDecoder = (map) => map['data'];
     Map<String, String> headers = {
-      "X-NCP-APIGW-API-KEY-ID": naverClientId, // 개인 클라이언트 아이디
-      "X-NCP-APIGW-API-KEY": naverClientSecret // 개인 시크릿 키
+      "X-NCP-APIGW-API-KEY-ID": SdkKeys.naverClientId, // 개인 클라이언트 아이디
+      "X-NCP-APIGW-API-KEY": SdkKeys.naverClientSecret // 개인 시크릿 키
     };
     return await httpClient.get('https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?request=coordsToaddr&coords=$lon,$lat&sourcecrs=epsg:4326&orders=addr&output=json', headers: headers);
+  }
+
+  Future<Response<KakaoAccount>> getKakaoAccount(String? token) async {
+    Map<String, String> headers = {'Authorization': 'Bearer $token'};
+    httpClient.addAuthenticator((Request request) async {
+      request.headers.addAll(headers);
+      return request;
+    });
+    httpClient.defaultDecoder = (map) => KakaoAccount.fromJson(map);
+    return await httpClient.get(URL_KAKAO_USER_ME);
   }
 
   Future<Response<Object>> signUp(UserCreate req) async {
