eafa373b314ccd85b8f4318059a296f1e1e6a119
jwhong <hju4287@naver.com>
2022-04-27 19:22:01 +0900

    Add kakao api url

--- lib/rest/api_keys.dart
+++ lib/rest/api_keys.dart
@@ -3,3 +3,6 @@ const String APP_SERVER_URL = 'localhost:8080/api/v1/app/';
 
 // Server Api Path
 const String PATH_USER_CREATE = 'user';
+
+// Kakao Api
+const String URL_KAKAO_USER_ME = 'https://kapi.kakao.com/v2/user/me';
