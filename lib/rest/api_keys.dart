d9226f0dda73c84955e371b1ce124479140bbad3
jwhong <hju4287@naver.com>
2022-04-28 20:18:57 +0900

    add api path

--- lib/rest/api_keys.dart
+++ lib/rest/api_keys.dart
@@ -2,6 +2,7 @@ const String SCHEME = 'http://';
 const String APP_SERVER_URL = 'localhost:8080/api/v1/app/';
 
 // Server Api Path
+const String PATH_LOGIN = 'auth';
 const String PATH_USER_CREATE = 'user';
 
 // Kakao Api
