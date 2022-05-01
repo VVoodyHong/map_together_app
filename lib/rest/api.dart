11e9688d4e7f2be79740ea218a268667c9e9a2ad
jwhong <hju4287@naver.com>
2022-05-01 20:48:29 +0900

    Add login & auth apies

--- lib/rest/api.dart
+++ lib/rest/api.dart
@@ -6,34 +6,44 @@ import 'package:map_together/model/jwt_authentication_response.dart';
 import 'package:map_together/model/kakao_account.dart';
 import 'package:map_together/model/request/login.dart';
 import 'package:map_together/model/request/user_create.dart';
+import 'package:map_together/model/user.dart';
 import 'package:map_together/rest/api_keys.dart';
 
 class API extends GetConnect {
   static API get to => Get.find();
+
+  String token = '';
 
   @override
   void onInit() {
     httpClient.defaultContentType = 'application/json';
     httpClient.timeout = Duration(seconds: 5);
     httpClient.addResponseModifier((request, response) {
-      print('rest result:: ${response.status.code}');
+      print('rest result:: ${request.url} ${response.status.code}');
     });
-
+    
     super.onInit();
   }
 
+  /*
+  naver map api
+  */
+
   // https://api.ncloud-docs.com/docs/ai-naver-mapsreversegeocoding-gc
   Future<Response<dynamic>> reverseGeocoding(lon ,lat) async {
-    // httpClient.defaultDecoder = (map) => map['data'];
     Map<String, String> headers = {
-      "X-NCP-APIGW-API-KEY-ID": SdkKeys.naverClientId, // 개인 클라이언트 아이디
-      "X-NCP-APIGW-API-KEY": SdkKeys.naverClientSecret // 개인 시크릿 키
+      "X-NCP-APIGW-API-KEY-ID": SdkKeys.naverClientId,
+      "X-NCP-APIGW-API-KEY": SdkKeys.naverClientSecret
     };
     return await httpClient.get('https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?request=coordsToaddr&coords=$lon,$lat&sourcecrs=epsg:4326&orders=addr&output=json', headers: headers);
   }
 
-  Future<Response<KakaoAccount>> getKakaoAccount(String? token) async {
-    Map<String, String> headers = {'Authorization': 'Bearer $token'};
+  /*
+  kakao login api
+  */
+
+  Future<Response<KakaoAccount>> getKakaoAccount(String? kakaoToken) async {
+    Map<String, String> headers = {'Authorization': 'Bearer $kakaoToken'};
     httpClient.addAuthenticator((Request request) async {
       request.headers.addAll(headers);
       return request;
@@ -41,6 +51,10 @@ class API extends GetConnect {
     httpClient.defaultDecoder = (map) => KakaoAccount.fromJson(map);
     return await httpClient.get(URL_KAKAO_USER_ME);
   }
+
+  /*
+  auth
+  */
 
   Future<Response<ApiResponse<JwtAuthenticationResponse>>> signIn(Login req) async {
     httpClient.defaultDecoder = (map) => ApiResponse<JwtAuthenticationResponse>.fromJson(map);
@@ -50,10 +64,41 @@ class API extends GetConnect {
     );
   }
 
-  Future<Response<Object>> signUp(UserCreate req) async {
+  Future<Response<ApiResponse<void>>> signUp(UserCreate req) async {
     return await httpClient.post(
-      SCHEME + APP_SERVER_URL + PATH_USER_CREATE,
-      body: req.toJson()
+      SCHEME + APP_SERVER_URL + PATH_SIGN_UP,
+      body: req.toJson(),
+    );
+  }
+
+  Future<Response<ApiResponse<JwtAuthenticationResponse>>> getNewAccessToken(String refreshToken) async {
+    Map<String, String> headers = {'authorization': 'Bearer $refreshToken'};
+    httpClient.defaultDecoder = (map) => ApiResponse<JwtAuthenticationResponse>.fromJson(map);
+    return await httpClient.get(
+      SCHEME + APP_SERVER_URL + PATH_ACCESSTOKEN,
+      headers: headers
+    );
+  }
+
+  Future<Response<ApiResponse<JwtAuthenticationResponse>>> refreshJwt() async {
+    Map<String, String> headers = {'authorization': 'Bearer $token'};
+    httpClient.defaultDecoder = (map) => ApiResponse<JwtAuthenticationResponse>.fromJson(map);
+    return await httpClient.get(
+      SCHEME + APP_SERVER_URL + PATH_REFRESH_JWT,
+      headers: headers
+    );
+  }
+  
+
+  /*
+  user
+  */
+  Future<Response<ApiResponse<User>>> getUser() async {
+    Map<String, String> headers = {'authorization': 'Bearer $token'};
+    httpClient.defaultDecoder = (map) => ApiResponse<User>.fromJson(map);
+    return await httpClient.get(
+      SCHEME + APP_SERVER_URL + PATH_USER,
+      headers: headers
     );
   }
 }
