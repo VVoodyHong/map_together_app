9e90232d80ed0bbfdb8b440cc62cfb03af28d3e4
jwhong <hju4287@naver.com>
2022-05-01 20:47:04 +0900

    Add user model

---
+++ lib/model/user.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User _$UserFromJson(Map<String, dynamic> json) => User(
+      idx: json['idx'] as int?,
+      nickname: json['nickname'] as String?,
+    );
+
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+      'idx': instance.idx,
+      'nickname': instance.nickname,
+    };
