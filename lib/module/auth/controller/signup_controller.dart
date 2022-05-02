52a026b3551c3e792c4075da9dab408856872a8c
jwhong <hju4287@naver.com>
2022-05-02 23:35:41 +0900

    Add sign up logic

--- lib/module/auth/controller/signup_controller.dart
+++ lib/module/auth/controller/signup_controller.dart
@@ -1,5 +1,54 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:map_together/model/request/user_create.dart';
+import 'package:map_together/rest/api.dart';
 
 class SignupX extends GetxController {
   static SignupX get to => Get.find();
+
+  RxBool isValidLoginId = false.obs;
+  RxBool isValidPassword = false.obs;
+  RxBool isValidConfirmPassword = false.obs;
+
+  TextEditingController loginIdController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+
+  void onChangeLoginId(String loginId) {
+    RegExp regExp = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    if(regExp.hasMatch(loginId)) {
+      isValidLoginId.value = true;
+    } else {
+      if(isValidLoginId.value) isValidLoginId.value = false;
+    }
+  }
+
+  void onChangePassword(String password) {
+    RegExp regExp = RegExp(r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?~^<>,.&+=])[A-Za-z\d$@$!%*#?~^<>,.&+=]{8,}$');
+    if(regExp.hasMatch(password)) {
+      isValidPassword.value = true;
+    } else {
+      if(isValidPassword.value) isValidPassword.value = false;
+    }
+  }
+
+  void onChangeConfirmPassword(String confirmPassword) {
+    print(passwordController.text.compareTo(confirmPasswordController.text));
+    if(passwordController.text.compareTo(confirmPasswordController.text) == 0) {
+      isValidConfirmPassword.value = true;
+    } else {
+      if(isValidConfirmPassword.value) isValidConfirmPassword.value = false;
+    }
+  }
+
+  void signUp() async {
+    UserCreate userCreate = UserCreate(
+      loginId: loginIdController.text,
+      nickname: loginIdController.text,
+      password: passwordController.text,
+    );
+    await API.to.signUp(userCreate).then((res) {
+      print(res.body);
+    });
+  }
 }
--- lib/module/auth/view/signup_screen.dart
+++ lib/module/auth/view/signup_screen.dart
@@ -1,19 +1,111 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:map_together/module/auth/controller/signup_controller.dart';
+import 'package:map_together/utils/constants.dart';
+import 'package:map_together/widget/base_app_bar.dart';
+import 'package:map_together/widget/base_button.dart';
+import 'package:map_together/widget/base_tff.dart';
+import 'package:map_together/widget/button_round.dart';
 
 class SignupScreen extends GetView<SignupX> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
+    return Obx(() => GestureDetector(
       onTap: ()=> FocusScope.of(context).unfocus(),
       child: Scaffold(
-        body: _body()
+        appBar: BaseAppBar(
+          title: '회원가입',
+          leading: BaseButton.iconButton(
+            iconData: Icons.arrow_back,
+            onPressed: () => Get.close(1)
+          )
+        ).init(),
+        body: _body(),
       ),
-    );
+    ));
   }
 
   Widget _body() {
-    return Container();
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Flexible(
+                  child: BaseTextFormField(
+                    controller: controller.loginIdController,
+                    label: '아이디',
+                    allowWhiteSpace: false,
+                    onChanged: (value) => controller.onChangeLoginId(value),
+                  ),
+                ),
+                ButtonRound(
+                  label: '중복확인',
+                  onTap: () {},
+                  buttonColor: controller.isValidLoginId.value ? MtColor.signature : MtColor.paleGrey,
+                  textColor: controller.isValidLoginId.value ? MtColor.white : MtColor.grey
+                )
+              ],
+            ).marginOnly(left: 15, right: 15, bottom: 20),
+            BaseTextFormField(
+              controller: controller.passwordController,
+              label: '비밀번호',
+              obscureText: true,
+              allowWhiteSpace: false,
+              maxLength: 15,
+              onChanged: (value) => controller.onChangePassword(value),
+            ).marginSymmetric(horizontal: 15),
+            BaseTextFormField(
+              controller: controller.confirmPasswordController,
+              label: '비밀번호 확인',
+              obscureText: true,
+              allowWhiteSpace: false,
+              maxLength: 15,
+              onChanged: (value) => controller.onChangeConfirmPassword(value),
+            ).marginSymmetric(horizontal: 15),
+            _confirmText(
+              text: '아이디는 이메일 형식으로 입력해주세요.',
+              isValid: controller.isValidLoginId.value
+            ),
+            _confirmText(
+              text: '문자, 숫자, 특수문자 포함 8자 이상으로 입력해주세요.',
+              isValid: controller.isValidPassword.value
+            ),
+            _confirmText(
+              text: '비밀번호를 한번 더 입력해여 확인해주세요.',
+              isValid: controller.isValidConfirmPassword.value
+            ),
+            ButtonRound(
+              label: '회원가입',
+              onTap: () {},
+              buttonColor: controller.isValidLoginId.value && controller.isValidPassword.value && controller.isValidConfirmPassword.value ? MtColor.signature : MtColor.paleGrey,
+              textColor: controller.isValidLoginId.value && controller.isValidPassword.value && controller.isValidConfirmPassword.value ? MtColor.white : MtColor.grey
+            ).marginAll(15),
+          ],
+        ),
+      )
+    );
+  }
+
+  Widget _confirmText({required String text, required bool isValid}) {
+    return Row(
+      children: [
+        Icon(
+          Icons.check,
+          color: isValid ? MtColor.signature : MtColor.grey
+        ).marginOnly(right: 5),
+        Expanded(
+          child: AutoSizeText(
+            text,
+            style: TextStyle(
+              color: isValid ? MtColor.black : MtColor.grey
+            ),
+            maxLines: 1,
+          ),
+        )
+      ],
+    ).marginOnly(left: 15, bottom: 5);
   }
 }
