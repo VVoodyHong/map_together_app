b85639ad1c3c87f8e1dbb5eaa66d94616b280def
jwhong <hju4287@naver.com>
2022-05-07 14:57:07 +0900

    Add finder box widget

---
+++ lib/widget/finder_box.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:map_together/utils/constants.dart';
+
+class FinderBox extends StatelessWidget{
+  final String? hintText;
+  final TextEditingController controller;
+  final VoidCallback onSearch;
+  final bool hasValue;
+  final ValueChanged<String> onChanged;
+  final VoidCallback clear;
+
+  FinderBox({
+    this.hintText,
+    required this.controller,
+    required this.onSearch,
+    required this.hasValue,
+    required this.onChanged,
+    required this.clear,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      textAlign: TextAlign.start,
+      cursorColor: MtColor.grey,
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        enabledBorder: _border(),
+        focusedBorder: _border(),
+        hintStyle: TextStyle(
+          color: MtColor.grey,
+        ),
+        filled: true,
+        fillColor: MtColor.paleGrey,
+        contentPadding: EdgeInsets.symmetric(horizontal: 20),
+        suffixIcon: hasValue
+        ? IconButton(
+            onPressed: clear,
+            icon: Icon(Icons.close, color: MtColor.grey),
+            splashColor: Colors.transparent,
+          )
+        : IconButton(
+            onPressed: () {search(context); },
+            icon: Icon(Icons.search, color: MtColor.grey),
+            splashColor: Colors.transparent,
+          )
+      ),
+      maxLength: 20,
+      onChanged: onChanged,
+      onEditingComplete: () {search(context); },
+      textInputAction: TextInputAction.done,
+    );
+  }
+
+  void search(context) {
+    onSearch();
+    FocusScope.of(context).unfocus();
+  }
+
+  OutlineInputBorder _border(){
+    return OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.circular(50)
+    );
+  }
+}
