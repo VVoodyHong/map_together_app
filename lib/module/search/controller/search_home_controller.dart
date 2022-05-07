4d5058160f2abdcb3e900caaae1742124a94598c
jwhong <hju4287@naver.com>
2022-05-07 15:02:03 +0900

    Add search controller

--- lib/module/search/controller/search_home_controller.dart
+++ lib/module/search/controller/search_home_controller.dart
@@ -1,6 +1,29 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:map_together/navigator/ui_state.dart';
 
 class SearchHomeX extends GetxController {
 
   static SearchHomeX get to => Get.find();
+
+  Rx<UiState> currentTab = UiState.SEARCH_USER.obs;
+  RxBool hasValue = false.obs;
+
+  TextEditingController textEditingController = TextEditingController();
+
+  void onChangeTab(UiState state) {
+    currentTab.value = state;
+  }
+
+  void doSearch() {
+  }
+
+  void onChangeSearch(String value) {
+    hasValue.value = textEditingController.text.isNotEmpty;
+  }
+
+  void clearSearch() {
+    textEditingController.clear();
+    hasValue.value = false;
+  }
 }
