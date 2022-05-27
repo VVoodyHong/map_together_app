2ba51902318b965f6ae6446a60a6aab70297b3d0
jwhong <hju4287@naver.com>
2022-05-27 21:28:42 +0900

    update user create model

--- lib/model/request/user_create.dart
+++ lib/model/request/user_create.dart
@@ -5,12 +5,10 @@ part 'user_create.g.dart';
 @JsonSerializable()
 class UserCreate {
   final String loginId;
-  final String nickname;
   final String password;
   
   UserCreate({
     required this.loginId,
-    required this.nickname,
     required this.password
   });
 
--- lib/model/request/user_create.g.dart
+++ lib/model/request/user_create.g.dart
@@ -8,13 +8,11 @@ part of 'user_create.dart';
 
 UserCreate _$UserCreateFromJson(Map<String, dynamic> json) => UserCreate(
       loginId: json['loginId'] as String,
-      nickname: json['nickname'] as String,
       password: json['password'] as String,
     );
 
 Map<String, dynamic> _$UserCreateToJson(UserCreate instance) =>
     <String, dynamic>{
       'loginId': instance.loginId,
-      'nickname': instance.nickname,
       'password': instance.password,
     };
