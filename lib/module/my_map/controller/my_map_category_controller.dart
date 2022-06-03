0b46ae6f96f9c325be83def7edcd7054841ac8a7
jwhong <hju4287@naver.com>
2022-06-03 18:39:44 +0900

    Add my map category screen

---
+++ lib/module/my_map/controller/my_map_category_controller.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:map_together/model/place_category/place_categories.dart';
+import 'package:map_together/model/place_category/place_category.dart';
+import 'package:map_together/model/place_category/place_category_create.dart';
+import 'package:map_together/model/response/api_response.dart';
+import 'package:map_together/model/type/place_category_type.dart';
+import 'package:map_together/rest/api.dart';
+import 'package:map_together/utils/utils.dart';
+
+class MyMapCategoryX extends GetxController {
+  static MyMapCategoryX get to => Get.find();
+
+  RxList<PlaceCategory> list = <PlaceCategory>[].obs;
+  RxInt selectedCategory = (-1).obs;
+  Rx<PlaceCategoryType> selectedMarker = PlaceCategoryType.NONE.obs;
+  TextEditingController nameController = TextEditingController();
+
+  @override
+  void onInit() {
+    super.onInit();
+    getPlaceCategories();
+  }
+
+  Future<void> getPlaceCategories() async {
+    ApiResponse<PlaceCategories> response = await API.to.getPlaceCategories();
+    if(response.success) {
+      list.addAll(response.data?.list ?? []);
+      nameController.clear();
+      selectedMarker.value = PlaceCategoryType.NONE;
+    } else {
+      print("getCategories error:: ${response.code} ${response.message}");
+      Utils.showToast(response.message);
+    }
+  }
+
+  void createPlaceCategory() async {
+    if(nameController.text.isEmpty) {
+      Utils.showToast("카테고리명을 입력해주세요.");
+      return;
+    } else if(selectedMarker.value == PlaceCategoryType.NONE) {
+      Utils.showToast("마커를 선택해주세요.");
+      return;
+    }
+    PlaceCategoryCreate placeCategoryCreate = PlaceCategoryCreate(
+      name: nameController.text,
+      type: selectedMarker.value,
+    );
+    ApiResponse<PlaceCategory> response = await API.to.createPlaceCategory(placeCategoryCreate);
+    if(response.success) {
+      if(response.data != null) {
+        list.add(response.data!);
+        Utils.showToast("카테고리가 추가되었습니다.");
+        Get.close(1);
+      } else {
+        Get.close(1);
+      }
+    } else {
+      print("getCategories error:: ${response.code} ${response.message}");
+      Utils.showToast(response.message);
+    }
+  }
+
+  void setSelectedCategory(int index) {
+    selectedCategory.value = index;
+  }
+
+  void setSelectedMarker(PlaceCategoryType marker) {
+    selectedMarker.value = marker;
+  }
+}
---
+++ lib/module/my_map/view/my_map_category_screen.dart
@@ -0,0 +1,220 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:map_together/model/type/place_category_type.dart';
+import 'package:map_together/module/my_map/controller/my_map_category_controller.dart';
+import 'package:map_together/utils/constants.dart';
+import 'package:map_together/widget/base_app_bar.dart';
+import 'package:map_together/widget/base_button.dart';
+import 'package:map_together/widget/base_tff.dart';
+import 'package:map_together/widget/bottom_sheet_modal.dart';
+import 'package:map_together/widget/button_round.dart';
+
+class MyMapCategoryScreen extends GetView<MyMapCategoryX> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() => Scaffold(
+      appBar: BaseAppBar(
+        title: '카테고리 선택',
+        leading: BaseButton.iconButton(
+          iconData: Icons.arrow_back,
+          onPressed: () => Get.close(1)
+        ),
+        actions: [
+          BaseButton.iconButton(
+            iconData: Icons.add,
+            onPressed: () {_showModal(context);}
+          ),
+        ]
+      ).init(),
+      body: _body()
+    ));
+  }
+  
+  void _showModal(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Obx(() => Dialog(
+          elevation: 0,
+          child: Container(
+            width: Get.width * 0.5,
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  '카테고리 추가',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500
+                  )
+                ),
+                BaseTextFormField(
+                  controller: controller.nameController,
+                  hintText: '카테고리명 입력',
+                  maxLength: 20,
+                ).marginSymmetric(horizontal: 15),
+                GestureDetector(
+                  onTap: () {
+                    BottomSheetModal.showWidget(
+                      context: context,
+                      widget: _gridMarkers()
+                    );
+                  },
+                  child: Container(
+                    color: MtColor.white,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          controller.selectedMarker.value == PlaceCategoryType.NONE ? '마커 선택' : '선택된 마커',
+                          style: TextStyle(
+                            fontSize: 18,
+                          )
+                        ),
+                        Spacer(),
+                        controller.selectedMarker.value == PlaceCategoryType.NONE ? Icon(
+                          Icons.keyboard_arrow_down
+                        ) : Container(
+                          height: 20,
+                          width: 20,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(Asset().getMarker(controller.selectedMarker.value.getValue()))
+                            )
+                          )
+                        )
+                      ],
+                    ).marginOnly(left: 15, right: 15, top: 15, bottom: 30),
+                  )
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: ButtonRound(
+                        label: '완료',
+                        onTap: controller.createPlaceCategory
+                      ).marginOnly(left: 10, right: 5)
+                    ),
+                    Expanded(
+                      child: ButtonRound(
+                        label: '취소',
+                        onTap: () {
+                          Get.close(1);
+                          controller.nameController.clear();
+                          controller.selectedMarker.value = PlaceCategoryType.NONE;
+                        },
+                        buttonColor: MtColor.paleGrey,
+                        textColor: MtColor.grey,
+                      ).marginOnly(left: 5, right: 10)
+                    )
+                  ]
+                )
+              ]
+            )
+          )
+        ));
+      }
+    ).then((_) => {
+      controller.nameController.clear(),
+      controller.selectedMarker.value = PlaceCategoryType.NONE
+    });
+  }
+
+  Widget _gridMarkers() {
+    return Container(
+      padding: EdgeInsets.all(30),
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 5,
+          childAspectRatio: 1.0,
+          crossAxisSpacing: 30,
+          mainAxisSpacing: 30,
+        ),
+        itemCount: Asset.markers.length,
+        itemBuilder: (BuildContext context, int index) {
+          return InkWell(
+            onTap: () {
+              controller.setSelectedMarker(Asset.markers[index]);
+              Get.close(1);
+            },
+            child: Container(
+              height: 50,
+              width: 50,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(Asset().getMarker(Asset.markers[index].getValue()))
+                )
+              )
+            ),
+          );
+        }
+      ),
+    );
+  }
+
+  Widget _body() {
+    return SafeArea(
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: controller.list.length,
+              itemBuilder: (BuildContext context, int index) {
+                return _imageTextField(index);
+              }
+            ),
+          ),
+          ButtonRound(
+              label: '완료',
+              onTap: () {},
+              buttonColor: controller.selectedCategory.value == -1 ? MtColor.paleGrey : null,
+              textColor: controller.selectedCategory.value == -1 ? MtColor.grey : null,
+            ).marginAll(15),
+        ],
+      )
+    );
+  }
+
+  Widget _imageTextField(int index) {
+    return Obx(() =>InkWell(
+      onTap: () {controller.setSelectedCategory(index);},
+      child: Container(
+        padding: EdgeInsets.all(15),
+        color: index == controller.selectedCategory.value ? MtColor.paleGrey.withOpacity(0.3) : null,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: 30,
+              width: 30,
+              margin: EdgeInsets.only(right: 10),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(Asset().getMarker(controller.list[index].type.getValue()))
+                )
+              )
+            ),
+            Text(
+              controller.list[index].name,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500
+              )
+            ),
+            Spacer(),
+            index == controller.selectedCategory.value ? Icon(
+              Icons.check,
+              color: MtColor.signature
+            ) : Container()
+          ],
+        ),
+      ),
+    ));
+  }
+}
