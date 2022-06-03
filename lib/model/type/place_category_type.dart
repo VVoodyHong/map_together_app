8bbe1ec889d6d7005e4dfbe5f7e05c5f8a2c8ccf
jwhong <hju4287@naver.com>
2022-06-03 18:38:45 +0900

    Add place category type enum columns

--- lib/model/type/place_category_type.dart
+++ lib/model/type/place_category_type.dart
@@ -1,5 +1,20 @@
 import 'package:json_annotation/json_annotation.dart';
 
 enum PlaceCategoryType{
+  @JsonValue('AIRPLANE') AIRPLANE,
+  @JsonValue('BEER') BEER,
+  @JsonValue('COFFEE') COFFEE,
+  @JsonValue('DESSERT') DESSERT,
   @JsonValue('HEART') HEART,
+  @JsonValue('MARKER') MARKER,
+  @JsonValue('RICE') RICE,
+  @JsonValue('SPORTS') SPORTS,
+  @JsonValue('STAR') STAR,
+  NONE
+}
+
+extension ParseToString on PlaceCategoryType {
+  String getValue() {
+    return toString().split('.').last;
+  }
 }
