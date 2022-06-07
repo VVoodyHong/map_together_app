fd301eb43caff580fb620dca976bc03762e483a6
jwhong <hju4287@naver.com>
2022-06-07 19:27:36 +0900

    Add tag field widget

---
+++ lib/widget/tag_field.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:map_together/utils/constants.dart';
+import 'package:textfield_tags/textfield_tags.dart';
+
+class TagField extends StatelessWidget {
+
+  final TextfieldTagsController tagsController;
+
+  TagField({
+    required this.tagsController
+  });
+  
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldTags(
+      textfieldTagsController: tagsController,
+      textSeparators: [' ', ','],
+      inputfieldBuilder: (BuildContext context, TextEditingController controller, FocusNode focusNode, String? error, Function(String)? onChanged, Function(String)? onSubmitted) {
+        return (BuildContext context, ScrollController scrollController, List<String> tags, Function(String) onDeleteTag) {
+          return TextField(
+            maxLength: 10,
+            controller: controller,
+            focusNode: focusNode,
+            decoration: InputDecoration(
+              counterText: '',
+              enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: MtColor.paleGrey
+                  )
+                ),
+                disabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: MtColor.paleGrey
+                  )
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: MtColor.signature
+                  )
+                ),
+              hintText: tagsController.hasTags ? '' : '태그 추가',
+              hintStyle: TextStyle(
+                color: MtColor.grey
+              ),
+              errorText: error,
+              prefixIconConstraints: BoxConstraints(maxWidth: Get.width * 0.75),
+              prefixIcon: tags.isNotEmpty ? NotificationListener<OverscrollIndicatorNotification>(
+                onNotification: (overscroll) {
+                  overscroll.disallowIndicator();
+                  return true;
+                },
+                child: SingleChildScrollView(
+                  controller: scrollController,
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: tags.map(
+                      (String tag) {
+                        return Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20),
+                            ),
+                            color: MtColor.signature
+                          ),
+                          margin: EdgeInsets.symmetric(horizontal: 5),
+                          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                          child: Row(
+                            mainAxisAlignment:
+                                MainAxisAlignment.spaceBetween,
+                            children: [
+                              InkWell(
+                                child: Text(
+                                  tag,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16
+                                  ),
+                                ),
+                                onTap: () {print("$tag selected");},
+                              ),
+                              SizedBox(width: 5),
+                              InkWell(
+                                child: Icon(
+                                  Icons.cancel,
+                                  size: 20,
+                                  color: MtColor.white
+                                ),
+                                onTap: () {
+                                  onDeleteTag(tag);
+                                },
+                              )
+                            ],
+                          ),
+                        );
+                      }
+                    ).toList()
+                  ),
+                ),
+              )
+            : null,
+            ),
+            onChanged: onChanged,
+            onSubmitted: onSubmitted,
+          );
+        };
+      }
+    );
+  }
+}
