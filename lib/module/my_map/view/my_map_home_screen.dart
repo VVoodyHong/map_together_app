e85aa11df5c0191edbca267b367bdbf2e270fda5
jwhong <hju4287@naver.com>
2022-06-07 19:28:39 +0900

    Add rating bar and tag

--- lib/module/my_map/view/my_map_home_screen.dart
+++ lib/module/my_map/view/my_map_home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:get/get.dart';
 import 'package:map_together/app.dart';
+import 'package:map_together/model/type/place_category_type.dart';
 import 'package:map_together/module/my_map/controller/my_map_home_controller.dart';
 import 'package:map_together/utils/constants.dart';
 import 'package:map_together/widget/base_button.dart';
@@ -12,6 +13,7 @@ import 'package:map_together/widget/bottom_nav.dart';
 import 'package:map_together/widget/bottom_sheet_modal.dart';
 import 'package:map_together/widget/button_profile.dart';
 import 'package:map_together/widget/base_app_bar.dart';
+import 'package:map_together/widget/button_round.dart';
 import 'package:map_together/widget/image_round.dart';
 
 class MyMapHomeScreen extends GetView<MyMapHomeX> {
@@ -28,7 +30,7 @@ class MyMapHomeScreen extends GetView<MyMapHomeX> {
             iconData: Icons.menu,
             onPressed: () => BottomSheetModal.showList(
               context: context,
-              listTiles: _listTiles()
+              listTiles: _listTiles(context)
             )
           )
         ]
@@ -122,15 +124,15 @@ class MyMapHomeScreen extends GetView<MyMapHomeX> {
             children: [
               ButtonProfile(
                 title: '장소',
-                number: '0'
+                number: controller.placeList.value.length
               ),
               ButtonProfile(
                 title: '팔로잉',
-                number: '0'
+                number: 0
               ),
               ButtonProfile(
                 title: '팔로워',
-                number: '0'
+                number: 0 
               ),
             ],
           ),
@@ -158,7 +160,7 @@ class MyMapHomeScreen extends GetView<MyMapHomeX> {
     );
   }
 
-  List<BaseListTile> _listTiles() {
+  List<BaseListTile> _listTiles(BuildContext context) {
     return [
       BaseListTile(
         title: '프로필 편집',
@@ -169,7 +171,106 @@ class MyMapHomeScreen extends GetView<MyMapHomeX> {
         title: '나의 맵 설정',
         onTap: controller.moveToSetting,
         icon: Icons.map_outlined,
+      ),
+      BaseListTile(
+        title: '카테고리 필터',
+        onTap: () {showCategoryModal(context);},
+        icon: Icons.filter_alt_outlined,
       )
     ];
+  }
+
+  void showCategoryModal(BuildContext context) {
+    Get.close(1);
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          elevation: 0,
+          child: Container(
+            width: Get.width * 0.5,
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  height: 300,
+                  child: ListView.builder(
+                    itemCount: controller.placeCategoryList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return _imageTextField(index);
+                    }
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: ButtonRound(
+                        label: '완료',
+                        onTap: controller.setSelectedPlaceCategory
+                      ).marginOnly(left: 10, right: 5)
+                    ),
+                    Expanded(
+                      child: ButtonRound(
+                        label: '취소',
+                        onTap: () {
+                          Get.close(1);
+                        },
+                        buttonColor: MtColor.paleGrey,
+                        textColor: MtColor.grey,
+                      ).marginOnly(left: 5, right: 10)
+                    )
+                  ]
+                )
+              ]
+            )
+          )
+        );
+      }
+    ).then((_) => {
+      controller.tempSelectedPlaceCategory.value = controller.selectedPlaceCategory.value
+    });
+  }
+
+  Widget _imageTextField(int index) {
+    return Obx(() =>InkWell(
+      onTap: () {controller.setTempSelectedPlaceCategory(index);},
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: 30,
+              width: 30,
+              margin: EdgeInsets.only(right: 10),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(Asset().getMarker(controller.placeCategoryList[index].type.getValue()))
+                )
+              )
+            ),
+            Expanded(
+              child: Text(
+                controller.placeCategoryList[index].name,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500
+                ),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            index == controller.tempSelectedPlaceCategory.value ? Icon(
+              Icons.check,
+              color: MtColor.signature
+            ) : Container()
+          ],
+        ),
+      ),
+    ));
   }
 }
