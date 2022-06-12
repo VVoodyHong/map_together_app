8cca880d3578fec8bfaa488c7e88b97ed8adce76
jwhong <hju4287@naver.com>
2022-06-12 23:11:39 +0900

    Add place screen logic

--- lib/module/place/controller/place_controller.dart
+++ lib/module/place/controller/place_controller.dart
@@ -1,10 +1,21 @@
 import 'package:carousel_slider/carousel_options.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:get/get.dart';
 import 'package:map_together/model/file/file.dart';
 import 'package:map_together/model/file/files.dart';
+import 'package:map_together/model/page/page.dart';
 import 'package:map_together/model/place/place.dart';
+import 'package:map_together/model/place_like/place_like.dart';
+import 'package:map_together/model/place_reply/place_replies.dart';
+import 'package:map_together/model/place_reply/place_reply_create.dart';
+import 'package:map_together/model/place_reply/place_reply_simple.dart';
 import 'package:map_together/model/response/api_response.dart';
+import 'package:map_together/model/tag/tag.dart';
+import 'package:map_together/model/tag/tags.dart';
+import 'package:map_together/model/type/place_category_type.dart';
 import 'package:map_together/rest/api.dart';
+import 'package:map_together/utils/constants.dart';
 import 'package:map_together/utils/utils.dart';
 
 class PlaceX extends GetxController {
@@ -12,13 +23,51 @@ class PlaceX extends GetxController {
   
   Rx<Place> place = (null as Place).obs;
   RxList<File> fileList = <File>[].obs;
+  RxList<Tag> tagList = <Tag>[].obs;
+  RxList<PlaceReplySimple> replyList = <PlaceReplySimple>[].obs;
+  RxList<Marker> markers = <Marker>[].obs;
   RxInt currentImage = 1.obs;
+  RxBool isLoading = false.obs;
+  RxBool isLastReply = false.obs;
+  RxInt replyPage = 1.obs;
+  RxDouble mapHeight = (200.0).obs;
+  RxBool isLike = false.obs;
+  RxInt totalLike = 0.obs;
+
+  TextEditingController replyController = TextEditingController();
+  ScrollController bodyScrollController = ScrollController();
+  ScrollController replyScrollController = ScrollController();
 
   @override
   void onInit() async {
     place.value = Get.arguments['place'];
     await getPlaceImage();
+    await getPlaceTag();
+    await getPlaceReply();
+    await getPlaceLike();
+    await setMarker();
+    bodyScrollController.addListener(listenScrollingBody);
+    replyScrollController.addListener(listenScrollingReply);
     super.onInit();
+  }
+
+  void listenScrollingBody() {
+    if(bodyScrollController.position.pixels > 0) {
+      mapHeight.value = 0;
+    } else {
+      mapHeight.value = 200;
+    }
+  }
+
+  void listenScrollingReply() async {
+    if (replyScrollController.position.atEdge) {
+      bool isTop = replyScrollController.position.pixels == 0;
+      if (!isTop && !isLastReply.value) {
+        isLoading.value = true;
+        await getPlaceReply();
+        isLoading.value = false;
+      };
+    }
   }
 
   Future<void> getPlaceImage() async {
@@ -31,7 +80,124 @@ class PlaceX extends GetxController {
     }
   }
 
+  Future<void> getPlaceTag() async {
+    ApiResponse<Tags> response =  await API.to.getPlaceTag(place.value.idx);
+    if(response.success) {
+      tagList.addAll(response.data?.list ?? []);
+    } else {
+      print("getPlaceTag error:: ${response.code} ${response.message}");
+      Utils.showToast(response.message);
+    }
+  }
+
+  Future<void> createPlaceReply() async {
+    if(replyController.text.trim().isEmpty) {
+      replyController.clear();
+      Utils.showToast('댓글을 입력해주세요.');
+      return;
+    }
+    isLoading.value = true;
+    PlaceReplyCreate placeReplyCreate = PlaceReplyCreate(
+      reply: replyController.text,
+      placeIdx: place.value.idx
+    );
+    ApiResponse<PlaceReplySimple> response = await API.to.createPlaceReply(placeReplyCreate);
+    if(response.success) {
+      replyList.add(response.data!);
+      replyController.clear();
+      scrollToBottom();
+    } else {
+      print("createPlaceReply error:: ${response.code} ${response.message}");
+      Utils.showToast(response.message);
+    }
+    isLoading.value = false;
+  }
+
+  Future<void> getPlaceReply() async {
+    RequestPage requestPage = RequestPage(
+      page: replyPage.value,
+      size: DefaultPage.size
+    );
+    ApiResponse<PlaceReplies> response =  await API.to.getPlaceReply(place.value.idx, requestPage);
+    if(response.success) {
+      replyList.addAll(response.data?.list ?? []);
+      replyPage++;
+      isLastReply.value = response.data!.last;
+    } else {
+      print("getPlaceReply error:: ${response.code} ${response.message}");
+      Utils.showToast(response.message);
+    }
+  }
+
+  Future<void> deletePlaceReply(int index) async {
+    ApiResponse<void> response = await API.to.deletePlaceReply(replyList[index].idx);
+    if(response.success) {
+      replyList.removeAt(index);
+      Utils.showToast('삭제가 완료되었습니다');
+      Get.close(1);
+    } else {
+      print("deletePlaceReply error:: ${response.code} ${response.message}");
+      Utils.showToast(response.message);
+    }
+  }
+
+  Future<void> createPlaceLike() async {
+    ApiResponse<void> response = await API.to.createPlaceLike(place.value.idx);
+    if(response.success) {
+      isLike.value = true;
+      totalLike.value = totalLike.value + 1;
+    } else {
+      print("createPlaceLike error:: ${response.code} ${response.message}");
+      Utils.showToast(response.message);
+    }
+  }
+
+  Future<void> getPlaceLike() async {
+    ApiResponse<PlaceLike> response = await API.to.getPlaceLike(place.value.idx);
+    if(response.success) {
+      isLike.value = response.data!.like;
+      totalLike.value = response.data!.totalLike;
+    } else {
+      print("getPlaceLike error:: ${response.code} ${response.message}");
+      Utils.showToast(response.message);
+    }
+  }
+
+  Future<void> deletePlaceLike() async {
+    ApiResponse<void> response = await API.to.deletePlaceLike(place.value.idx);
+    if(response.success) {
+      isLike.value = false;
+      totalLike.value = totalLike.value - 1;
+    } else {
+      print("deletePlaceLike error:: ${response.code} ${response.message}");
+      Utils.showToast(response.message);
+    }
+  }
+
+  void scrollToBottom() {
+    double replyPosition = replyScrollController.position.maxScrollExtent;
+    double bodyPosition = bodyScrollController.position.maxScrollExtent;
+    replyScrollController.animateTo(replyPosition + 100, duration: Duration(milliseconds: 200), curve: Curves.easeIn);
+    bodyScrollController.animateTo(bodyPosition + 100, duration: Duration(milliseconds: 200), curve: Curves.easeIn);
+  }
+
   void onImageChanged(int index, CarouselPageChangedReason reason) {
     currentImage.value = index + 1;
+  }
+
+  Future<void> setMarker() async {
+    LatLng position = LatLng(
+      place.value.lat,
+      place.value.lng
+    );
+    markers.add(
+      Marker(
+        markerId: position.json.toString(),
+        position: position,
+        height: 20,
+        width: 20,
+        icon: await OverlayImage.fromAssetImage(assetName: Asset().getMarker(place.value.category.type.getValue()))
+      )
+    );
   }
 }
--- lib/module/place/view/place_screen.dart
+++ lib/module/place/view/place_screen.dart
@@ -1,88 +1,381 @@
+// ignore_for_file: invalid_use_of_protected_member
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:get/get.dart';
+import 'package:map_together/app.dart';
 import 'package:map_together/model/file/file.dart';
+import 'package:map_together/model/tag/tag.dart';
 import 'package:map_together/module/place/controller/place_controller.dart';
 import 'package:map_together/utils/constants.dart';
+import 'package:map_together/utils/utils.dart';
 import 'package:map_together/widget/base_app_bar.dart';
 import 'package:map_together/widget/base_button.dart';
+import 'package:map_together/widget/base_list_tile.dart';
+import 'package:map_together/widget/bottom_sheet_modal.dart';
+import 'package:map_together/widget/image_round.dart';
+import 'package:map_together/widget/rating_bar.dart';
 
 class PlaceScreen extends GetView<PlaceX> {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() => Scaffold(
-      appBar: BaseAppBar(
-        title: '장소',
-        leading: BaseButton.iconButton(
-          iconData: Icons.arrow_back,
-          onPressed: () => Get.close(1)
+    return Obx(() => GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: BaseAppBar(
+          title: App.to.user.value.nickname ?? '',
+          leading: BaseButton.iconButton(
+            iconData: Icons.arrow_back,
+            onPressed: () => Get.close(1)
+          ),
+          onPressedTitle: () {print("@@");},
+          centerTitle: false
+        ).init(),
+        body: Column(
+          children: [
+            Expanded(child: _body()),
+            _replyBar()
+          ],
         ),
-      ).init(),
-      body: _body(),
+      ),
     ));
   }
 
   Widget _body() {
+    if(controller.bodyScrollController.position.pixels == 0) controller.mapHeight.value = 200;
     return SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            controller.fileList.isNotEmpty ? Stack(
-              children: [
-                CarouselSlider(
-                  items: _carouselItems(),
-                  options: CarouselOptions(
-                    viewportFraction: 1.0,
-                    aspectRatio: 1,
-                    enableInfiniteScroll: false,
-                    enlargeCenterPage: true,
-                    onPageChanged: controller.onImageChanged
-                  )
-                ),
-                controller.fileList.length > 1 ? Positioned(
-                  bottom: 0,
-                  right: 0,
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                    margin: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: MtColor.black.withOpacity(0.3),
-                      borderRadius: BorderRadius.all(Radius.circular(20))
-                    ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _naverMap(),
+          _contents()
+        ],
+      ),
+    );
+  }
+
+  Widget _naverMap() {
+    return SizedBox(
+      height: controller.mapHeight.value,
+      child: NaverMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            controller.place.value.lat,
+            controller.place.value.lng
+          ),
+          zoom: 15,
+        ),
+        markers: controller.markers.value
+      )
+    );
+  }
+
+  Widget _contents() {
+    return Expanded(
+      child: NotificationListener<OverscrollIndicatorNotification>(
+        onNotification: (overscroll) {
+          overscroll.disallowIndicator();
+          return true;
+        },
+        child: SingleChildScrollView(
+          controller: controller.bodyScrollController,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 200 - controller.mapHeight.value
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
                     child: Text(
-                      '${controller.currentImage} / ${controller.fileList.length}',
+                      controller.place.value.name,
                       style: TextStyle(
-                        color: Colors.white
-                      ),
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500
+                      )
                     ),
                   ),
-                ) : Container()
-              ],
-            ) : Container(),
-          ],
+                  BaseButton.iconButton(
+                    iconData: controller.isLike.value ? Icons.favorite : Icons.favorite_outline,
+                    color: MtColor.red.withOpacity(0.8),
+                    size: 30,
+                    boxConstraints: BoxConstraints(
+                      minWidth: 30,
+                      minHeight: 30
+                    ),
+                    onPressed: () {
+                      controller.isLike.value ? controller.deletePlaceLike() : controller.createPlaceLike();
+                    }
+                  ),
+                ],
+              ).marginOnly(top: 15),
+              Row(
+                children: [
+                  RatingBar(
+                    initialRating: controller.place.value.favorite,
+                    onRatingUpdate: () {},
+                    icon: Icon(
+                      Icons.favorite,
+                      color: MtColor.signature,
+                    ),
+                    itemSize: 20,
+                    horizonItemPadding: 0,
+                  ),
+                  Text(
+                    Utils.trimDate(controller.place.value.createAt),
+                    style: TextStyle(
+                      color: MtColor.grey
+                    ),
+                  ).marginOnly(left: 5)
+                ],
+              ).marginOnly(top: 10),
+              Text(
+                controller.place.value.address,
+                style: TextStyle(
+                  color: MtColor.paleBlack,
+                  fontSize: 16
+                )
+              ).marginOnly(top: 10),
+              (controller.place.value.description ?? '').isNotEmpty ? Text(
+                controller.place.value.description ?? '',
+                style: TextStyle(
+                  fontSize: 16
+                )
+              ).marginOnly(top: 15) : Container(),
+              _images(),
+              _tags(),
+              controller.totalLike.value != 0 ? Row(
+                children: [
+                  Text(
+                    '${controller.totalLike.value}명',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                  Text(
+                    '이 좋아합니다.',
+                    style: TextStyle(
+                      fontSize: 16,
+                    )
+                  )
+                ],
+              ).marginOnly(top: 15, bottom: 15) : Container().marginOnly(top: 15, bottom: 15),
+              _replys()
+            ],
+          ).marginSymmetric(horizontal: 15),
         ),
       ),
     );
+  }
+
+  Widget _images() {
+    return controller.fileList.isNotEmpty ? Stack(
+      children: [
+        CarouselSlider(
+          items: _carouselItems(),
+          options: CarouselOptions(
+            viewportFraction: 1.0,
+            aspectRatio: 1,
+            enableInfiniteScroll: false,
+            enlargeCenterPage: true,
+            onPageChanged: controller.onImageChanged
+          )
+        ),
+        controller.fileList.length > 1 ? Positioned(
+          bottom: 0,
+          right: 0,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: MtColor.black.withOpacity(0.3),
+              borderRadius: BorderRadius.all(Radius.circular(20))
+            ),
+            child: Text(
+              '${controller.currentImage} / ${controller.fileList.length}',
+              style: TextStyle(
+                color: Colors.white
+              ),
+            ),
+          ),
+        ) : Container()
+      ],
+    ).marginOnly(top: 15) : Container();
+  }
+
+  Widget _tags() {
+    List<Widget> _list = [];
+    for (Tag tag in controller.tagList) {
+      _list.add(
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          margin: EdgeInsets.only(right: 10, top: 5),
+          decoration: BoxDecoration(
+            color: MtColor.signature,
+            borderRadius: BorderRadius.all(Radius.circular(20))
+          ),
+          child: Text(
+            tag.name ?? '',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16
+            ),
+          ),
+        )
+      );
+    }
+    return controller.tagList.isNotEmpty ? Wrap(
+      children: _list,
+    ).marginOnly(top: 10) : Container();
   }
 
   List<Widget> _carouselItems() {
     List<Widget> _list = [];
     for (File file in controller.fileList) {
       _list.add(
-        Container(
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: NetworkImage(file.url)
-            )
+        ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          child: Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(file.url)
+              )
+            ),
           ),
         )
       );
     }
     return _list;
+  }
+
+  Widget _replys() {
+    return SizedBox(
+      height: controller.replyList.isNotEmpty ? 300 : 0,
+      child: Stack(
+        children: [
+          ListView.builder(
+            controller: controller.replyScrollController,
+            shrinkWrap: true,
+            itemCount: controller.replyList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return _replyCard(context, index).marginOnly(bottom: 15);
+            },
+          ),
+          Utils.showLoading(isLoading: controller.isLoading.value)
+        ],
+      ),
+    );
+  }
+
+  Widget _replyCard(BuildContext context, int index) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ImageRound(
+          imagePath: controller.replyList[index].userProfileImg,
+          imageSize: 60
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                controller.replyList[index].userNickname,
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              Text(
+                Utils.trimDate(controller.replyList[index].createAt),
+                style: TextStyle(
+                  color: MtColor.grey
+                ),
+              ).marginOnly(top: 5),
+              Text(
+                controller.replyList[index].reply,
+                style: TextStyle(
+                  fontSize: 16
+                ),
+              ).marginOnly(top: 5),
+            ],
+          ).marginOnly(left: 10),
+        ),
+        BaseButton.iconButton(
+          iconData: Icons.more_vert,
+          onPressed: () => BottomSheetModal.showList(
+            context: context,
+            listTiles: _listTiles(context, index)
+          )
+        )
+      ],
+    );
+  }
+
+  List<BaseListTile> _listTiles(BuildContext context, int index) {
+    return [
+      BaseListTile(
+        title: '삭제',
+        onTap: () {controller.deletePlaceReply(index);},
+        icon: Icons.delete
+      ),
+    ];
+  }
+
+  Widget _replyBar() {
+    return Container(
+      padding: EdgeInsets.only(left: 10),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextFormField(
+              controller: controller.replyController,
+              keyboardType: TextInputType.text,
+              maxLines: 1,
+              maxLength: 500,
+              autofocus: false,
+              decoration: InputDecoration(
+                counterText: ''     ,
+                focusColor: MtColor.white,
+                hintText: '댓글 쓰기',
+                hintStyle: TextStyle(
+                    color: MtColor.grey,
+                    fontSize: 16
+                ),
+                filled: true,
+                fillColor: MtColor.paleGrey,
+                enabledBorder: _border(),
+                focusedBorder: _border(),
+                isDense: true,
+                contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              ),
+              onFieldSubmitted: (value) {controller.createPlaceReply();},
+            ),
+          ),
+          BaseButton.iconButton(
+            iconData: Icons.send,
+            color: MtColor.signature,
+            onPressed: () {
+              FocusManager.instance.primaryFocus?.unfocus();
+              controller.createPlaceReply();
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  OutlineInputBorder _border(){
+    return OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.circular(6)
+    );
   }
 }
