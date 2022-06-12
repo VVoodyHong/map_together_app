f002ac8a88ec5e2d69ba3ffc463c0df291991925
jwhong <hju4287@naver.com>
2022-06-12 23:11:18 +0900

    Add tag mdeol

---
+++ lib/model/tag/tag.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'tag.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Tag _$TagFromJson(Map<String, dynamic> json) => Tag(
+      idx: json['idx'] as int,
+      name: json['name'] as String?,
+    );
+
+Map<String, dynamic> _$TagToJson(Tag instance) => <String, dynamic>{
+      'idx': instance.idx,
+      'name': instance.name,
+    };
