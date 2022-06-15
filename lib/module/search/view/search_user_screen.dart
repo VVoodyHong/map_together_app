d96ed87f43af8c2ee8443d0c3ccb733228b05543
jwhong <hju4287@naver.com>
2022-06-15 22:06:58 +0900

    Add empty view

--- lib/module/search/view/search_user_screen.dart
+++ lib/module/search/view/search_user_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:map_together/app.dart';
 import 'package:map_together/module/search/controller/search_home_controller.dart';
 import 'package:map_together/utils/utils.dart';
+import 'package:map_together/widget/empty_view.dart';
 import 'package:map_together/widget/image_round.dart';
 
 class SearchUserScreen extends GetView<SearchHomeX> {
@@ -11,13 +11,13 @@ class SearchUserScreen extends GetView<SearchHomeX> {
   Widget build(BuildContext context) {
     return Obx(() => Stack(
       children: [
-        SingleChildScrollView(
+        controller.userList.isNotEmpty ? SingleChildScrollView(
           controller: controller.userScrollController,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: _userList()
           ),
-        ),
+        ) : controller.isSearched.value ? EmptyView(text: '검색 결과가 존재하지 않습니다.') : Container(),
         Utils.showLoading(isLoading: controller.isLoading.value)
       ],
     )).marginSymmetric(horizontal: 20);
@@ -26,46 +26,44 @@ class SearchUserScreen extends GetView<SearchHomeX> {
   List<Widget> _userList() {
     List<Widget> _list = [];
     for (var user in controller.userList) {
-      if(user.idx != App.to.user.value.idx) {
-        _list.add(
-          InkWell(
-            onTap: () {controller.moveToUserHome(user.idx);},
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ImageRound(
-                  imagePath: user.profileImg,
-                  imageSize: 70
-                ),
-                Expanded(
-                  child: SizedBox(
-                    height: 70,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          user.nickname,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600
-                          ),
+      _list.add(
+        InkWell(
+          onTap: () {controller.moveToUserHome(user.idx);},
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ImageRound(
+                imagePath: user.profileImg,
+                imageSize: 70
+              ),
+              Expanded(
+                child: SizedBox(
+                  height: 70,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        user.nickname,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600
                         ),
-                        Text(
-                          user.name ?? '',
-                          style: TextStyle(
-                            fontSize: 15
-                          ),
-                        ).marginOnly(top: 5),
-                      ],
-                    )
-                  ).marginOnly(left: 10),
-                ),
-              ],
-            ).marginOnly(top: 15),
-          )
-        );
-      }
+                      ),
+                      Text(
+                        user.name ?? '',
+                        style: TextStyle(
+                          fontSize: 15
+                        ),
+                      ).marginOnly(top: 5),
+                    ],
+                  )
+                ).marginOnly(left: 10),
+              ),
+            ],
+          ).marginOnly(top: 15),
+        )
+      );
     }
     return _list;
   }
