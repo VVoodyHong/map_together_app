142706f0232582f3ba559a3847376de8f618414f
jwhong <hju4287@naver.com>
2022-06-17 19:32:23 +0900

    Add follow screen

---
+++ lib/module/follow/view/follower_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:map_together/app.dart';
+import 'package:map_together/model/follow/follow_simple.dart';
+import 'package:map_together/module/follow/controller/follow_home_controller.dart';
+import 'package:map_together/widget/empty_view.dart';
+import 'package:map_together/widget/image_round.dart';
+
+class FollowerScreen extends StatelessWidget {
+
+  late String uniqueTag;
+  late FollowHomeX controller;
+
+  FollowerScreen(String _tag, FollowHomeX _controller) {
+    uniqueTag = _tag;
+    controller = _controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() => controller.followerList.isNotEmpty ? SingleChildScrollView(
+      controller: controller.followerScrollController,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: _userList()
+      )
+    ) : !controller.isLoading.value ? EmptyView(text: '팔로워가 없습니다.') : Container()).marginSymmetric(horizontal: 20);
+  }
+
+  List<Widget> _userList() {
+    List<Widget> _list = [];
+    for (FollowSimple follow in controller.followerList) {
+      _list.add(
+        InkWell(
+          onTap: () {follow.idx != App.to.user.value.idx ? controller.moveToUserHome(follow.idx) : () {};},
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ImageRound(
+                imagePath: follow.profileImg,
+                imageSize: 70
+              ),
+              Expanded(
+                child: SizedBox(
+                  height: 70,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        follow.nickname,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600
+                        ),
+                      ),
+                      Text(
+                        follow.name ?? '',
+                        style: TextStyle(
+                          fontSize: 15
+                        ),
+                      ).marginOnly(top: 5),
+                    ],
+                  )
+                ).marginOnly(left: 10),
+              ),
+            ],
+          ).marginOnly(top: 15),
+        )
+      );
+    }
+    return _list;
+  }
+}
