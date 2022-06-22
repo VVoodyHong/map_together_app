dba91be4b80301653c4ef386f2b6ab986e91597d
jwhong <hju4287@naver.com>
2022-06-22 15:37:21 +0900

    Add ignore warning

--- lib/common/photo_uploader.dart
+++ lib/common/photo_uploader.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: invalid_use_of_visible_for_testing_member
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_cropper/image_cropper.dart';
