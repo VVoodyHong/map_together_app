8ba0f02b61c71fc6c792c006ac4ab92ed5e0833b
jwhong <hju4287@naver.com>
2022-06-23 19:36:31 +0900

    Update image uploader for place update

--- lib/widget/image_uploader.dart
+++ lib/widget/image_uploader.dart
@@ -1,28 +1,31 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:map_together/model/file/file.dart' as mapto;
 import 'package:map_together/utils/constants.dart';
 
 class ImageUploader extends StatelessWidget {
 
   final double? height;
   final double? width;
-  final List<File> images;
-  final VoidCallback onCreate;
+  final List<File>? files;
+  final List<mapto.File>? images;
+  final VoidCallback? onCreate;
   final Function(int index) onDelete;
 
   ImageUploader({
     this.height,
     this.width,
-    required this.images,
-    required this.onCreate,
+    this.files,
+    this.images,
+    this.onCreate,
     required this.onDelete
   });
 
   @override
   Widget build(BuildContext context) {
     
-    return Column(
+    return files != null ? Column(
       children: [
         Container(
           height: height ?? 150,
@@ -36,8 +39,8 @@ class ImageUploader extends StatelessWidget {
               overscroll.disallowIndicator();
               return true;
             },
-            child: images.isNotEmpty ? ListView.builder(
-              itemCount: images.length,
+            child: files!.isNotEmpty ? ListView.builder(
+              itemCount: files!.length,
               scrollDirection: Axis.horizontal,
               itemBuilder: (BuildContext context, int index) {
                 return Row(
@@ -54,7 +57,7 @@ class ImageUploader extends StatelessWidget {
                               borderRadius: BorderRadius.circular(20),
                               image: DecorationImage(
                                 fit: BoxFit.cover,
-                                image: FileImage(images[index])
+                                image: FileImage(files![index])
                               )
                             ),
                           ),
@@ -83,7 +86,7 @@ class ImageUploader extends StatelessWidget {
                         ],
                       ),
                     ),
-                    (images.length < 10 && index == images.length - 1) ? GestureDetector(
+                    (files!.length < 10 && index == files!.length - 1) ? GestureDetector(
                       onTap: onCreate,
                       child: SizedBox(
                         height: height ?? 150,
@@ -146,11 +149,79 @@ class ImageUploader extends StatelessWidget {
           alignment: Alignment.centerRight,
           margin: EdgeInsets.only(top: 5),
           child: Text(
-            '(${(images.length)}/10)',
+            '(${(files!.length)}/10)',
             style: TextStyle(
               fontSize: 16
             )
           ),
+        )
+      ],
+    ) : Column(
+      children: [
+        Container(
+          height: height ?? 150,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: MtColor.paleGrey.withOpacity(0.3),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: NotificationListener<OverscrollIndicatorNotification>(
+            onNotification: (overscroll) {
+              overscroll.disallowIndicator();
+              return true;
+            },
+            child: images!.isNotEmpty ? ListView.builder(
+              itemCount: images!.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (BuildContext context, int index) {
+                return Row(
+                  children: [
+                    SizedBox(
+                      height: height ?? 150,
+                      width: width ?? 150,
+                      child: Stack(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.all(15),
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(images![index].url)
+                              )
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {onDelete(index);},
+                            child: Container(
+                              margin: EdgeInsets.only(left: (width ?? 150) - 30, top: 5),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: MtColor.paleBlack,
+                                  borderRadius: BorderRadius.circular(20)
+                                ),
+                                height: 25,
+                                width: 25,
+                                alignment: Alignment.topRight,
+                                child: Center(
+                                  child: Icon(
+                                    Icons.close,
+                                    color: MtColor.white,
+                                    size: 20,
+                                  ),
+                                ),
+                              )
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                );
+              }
+            ) : Container()
+          )
         )
       ],
     );
