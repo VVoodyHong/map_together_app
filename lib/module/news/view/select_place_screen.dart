7591c128dec271351048ca5bc9d5cd187d6099da
jwhong <hju4287@naver.com>
2022-06-30 19:34:55 +0900

    Update ignore warning

--- lib/module/news/view/select_place_screen.dart
+++ lib/module/news/view/select_place_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: invalid_use_of_protected_member
+
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:get/get.dart';
