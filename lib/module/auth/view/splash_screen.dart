be791d195af374d1908d3e6a62230e7c8bf5b7a6
jwhong <hju4287@naver.com>
2022-07-11 19:28:51 +0900

    Update splash screen view

--- lib/module/auth/view/splash_screen.dart
+++ lib/module/auth/view/splash_screen.dart
@@ -1,4 +1,6 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:map_together/utils/constants.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -9,17 +11,30 @@ class SplashScreen extends StatelessWidget {
 
   Widget _body() {
     return SafeArea(
-      child: Container(
-        alignment: Alignment.center,
-        child: Text(
-          "MAPTOGETHER",
-          style: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-            color: MtColor.signature
-          )
-        ),
-      )
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 100,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.contain,
+                image: AssetImage(Asset.logoSignature)
+              )
+            ),
+          ).marginOnly(bottom: 10),
+          AutoSizeText(
+            "MAPTOGETHER",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: MtColor.signature
+            ),
+            maxLines: 1,
+          ),
+        ],
+      ),
     );
   }
 }
