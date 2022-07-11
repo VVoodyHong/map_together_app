fc12a5b306c9a72027ef0d85c9908881bb83b187
jwhong <hju4287@naver.com>
2022-07-11 19:29:16 +0900

    Add email authentication to sign up

--- lib/module/auth/controller/signup_controller.dart
+++ lib/module/auth/controller/signup_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:map_together/model/auth/auth_email.dart';
 import 'package:map_together/model/response/api_response.dart';
 import 'package:map_together/model/type/exist_type.dart';
 import 'package:map_together/model/user/user_create.dart';
@@ -9,17 +10,25 @@ import 'package:map_together/utils/utils.dart';
 class SignupX extends GetxController {
   static SignupX get to => Get.find();
 
+  RxBool isLoading = false.obs;
   RxBool isValidLoginId = false.obs;
   RxBool availableLoginId = false.obs;
   RxBool isValidPassword = false.obs;
   RxBool isValidConfirmPassword = false.obs;
+  RxBool isVaildAuthCode = false.obs;
+  RxBool isSend = false.obs;
+  RxString authCode = ''.obs;
 
   TextEditingController loginIdController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   TextEditingController confirmPasswordController = TextEditingController();
+  TextEditingController authEmailController = TextEditingController();
 
   void onChangeLoginId(String loginId) {
     availableLoginId.value = false;
+    isSend.value = false;
+    isVaildAuthCode.value = false;
+    if(authEmailController.text.isNotEmpty) authEmailController.clear();
     RegExp regExp = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
     if(regExp.hasMatch(loginId)) {
       isValidLoginId.value = true;
@@ -59,6 +68,30 @@ class SignupX extends GetxController {
       availableLoginId.value = false;
       Exception("checkExistUser error:: ${response.code} ${response.message}");
       Utils.showToast(response.message);
+    }
+  }
+
+  void authEmail() async {
+    AuthEmail authEmail = AuthEmail(email: loginIdController.text);
+    isLoading.value = true;
+    ApiResponse<AuthEmail> response = await API.to.authEmail(authEmail);
+    if(response.success) {
+      authCode.value = response.data!.code ?? '';
+      isSend.value = true;
+      Utils.showToast('인증번호가 발송되었습니다.');
+    } else {
+      Exception("authEmail error:: ${response.code} ${response.message}");
+      Utils.showToast(response.message);
+    }
+    isLoading.value = false;
+  }
+
+  void checkAuthCode() async {
+    if(authEmailController.text == authCode.value) {
+      Utils.showToast('인증이 완료되었습니다.');
+      isVaildAuthCode.value = true;
+    } else {
+      Utils.showToast('인증 번호가 일치하지 않습니다.');
     }
   }
 
--- lib/module/auth/view/signup_screen.dart
+++ lib/module/auth/view/signup_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:map_together/module/auth/controller/signup_controller.dart';
 import 'package:map_together/utils/constants.dart';
+import 'package:map_together/utils/utils.dart';
 import 'package:map_together/widget/base_app_bar.dart';
 import 'package:map_together/widget/base_button.dart';
 import 'package:map_together/widget/base_tff.dart';
@@ -11,18 +12,23 @@ import 'package:map_together/widget/button_round.dart';
 class SignupScreen extends GetView<SignupX> {
   @override
   Widget build(BuildContext context) {
-    return Obx(() => GestureDetector(
-      onTap: ()=> FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        appBar: BaseAppBar(
-          title: '회원가입',
-          leading: BaseButton.iconButton(
-            iconData: Icons.arrow_back,
-            onPressed: () => Get.close(1)
-          )
-        ).init(),
-        body: _body(),
-      ),
+    return Obx(() => Stack(
+      children: [
+        GestureDetector(
+          onTap: ()=> FocusScope.of(context).unfocus(),
+          child: Scaffold(
+            appBar: BaseAppBar(
+              title: '회원가입',
+              leading: BaseButton.iconButton(
+                iconData: Icons.arrow_back,
+                onPressed: () => Get.close(1)
+              )
+            ).init(),
+            body: _body(),
+          ),
+        ),
+        Utils.showLoading(isLoading: controller.isLoading.value)
+      ],
     ));
   }
 
@@ -50,6 +56,30 @@ class SignupScreen extends GetView<SignupX> {
                 )
               ],
             ).marginOnly(left: 15, right: 15, bottom: 20),
+            controller.availableLoginId.value ?
+              !controller.isVaildAuthCode.value ?
+                Row(
+                  children: [
+                    Flexible(
+                      child: BaseTextFormField(
+                        controller: controller.authEmailController,
+                        label: '인증번호',
+                        maxLength: 64,
+                        allowWhiteSpace: false,
+                        onChanged: (value) {}
+                      ),
+                    ),
+                    !controller.isSend.value ? ButtonRound(
+                      label: '인증번호 전송',
+                      onTap: controller.authEmail,
+                    ) : ButtonRound(
+                      label: '인증하기',
+                      onTap: controller.checkAuthCode,
+                    )
+                  ],
+                ).marginOnly(left: 15, right: 15, bottom: 20)
+              : Container()
+            : Container(),
             BaseTextFormField(
               controller: controller.passwordController,
               label: '비밀번호',
@@ -75,6 +105,10 @@ class SignupScreen extends GetView<SignupX> {
               isValid: controller.availableLoginId.value
             ),
             _confirmText(
+              text: '이메일 인증을 완료해주세요.',
+              isValid: controller.isVaildAuthCode.value
+            ),
+            _confirmText(
               text: '문자, 숫자, 특수문자 포함 8자 이상으로 입력해주세요.',
               isValid: controller.isValidPassword.value
             ),
@@ -84,9 +118,9 @@ class SignupScreen extends GetView<SignupX> {
             ),
             ButtonRound(
               label: '회원가입',
-              onTap: controller.isValidLoginId.value && controller.isValidPassword.value && controller.isValidConfirmPassword.value && controller.availableLoginId.value ? controller.signUp : () {},
-              buttonColor: controller.isValidLoginId.value && controller.isValidPassword.value && controller.isValidConfirmPassword.value && controller.availableLoginId.value ? MtColor.signature : MtColor.paleGrey,
-              textColor: controller.isValidLoginId.value && controller.isValidPassword.value && controller.isValidConfirmPassword.value && controller.availableLoginId.value ? MtColor.white : MtColor.grey
+              onTap: controller.isValidLoginId.value && controller.isValidPassword.value && controller.isVaildAuthCode.value && controller.isValidConfirmPassword.value && controller.availableLoginId.value ? controller.signUp : () {},
+              buttonColor: controller.isValidLoginId.value && controller.isValidPassword.value && controller.isVaildAuthCode.value && controller.isValidConfirmPassword.value && controller.availableLoginId.value ? MtColor.signature : MtColor.paleGrey,
+              textColor: controller.isValidLoginId.value && controller.isValidPassword.value && controller.isVaildAuthCode.value && controller.isValidConfirmPassword.value && controller.availableLoginId.value ? MtColor.white : MtColor.grey
             ).marginAll(15),
           ],
         ),
